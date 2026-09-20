2471288ae1769a1854da0799992f6984ef4006e4
TNC7-Studio <studiotnc7@gmail.com>
2026-09-20 10:16:46 +0700

    Update service worker cache version to v36

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Ganti angka versi ini (misal: v3, v4, dst) SETIAP KALI Anda mengupdate index.html
-const CACHE_NAME = 'tnc7-visualizer-v35'; 
+const CACHE_NAME = 'tnc7-visualizer-v36'; 
 
 const urlsToCache = [
   './',
