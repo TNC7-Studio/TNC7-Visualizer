3f7993c393c4cf8da5f9d1df50f21a4a9316d64c
TNC7-Studio <studiotnc7@gmail.com>
2026-09-23 15:42:44 +0700

    Update service worker cache version to v41

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Ganti angka versi ini (misal: v3, v4, dst) SETIAP KALI Anda mengupdate index.html
-const CACHE_NAME = 'tnc7-visualizer-v40'; 
+const CACHE_NAME = 'tnc7-visualizer-v41'; 
 
 const urlsToCache = [
   './',
